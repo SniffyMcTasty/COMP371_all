0fd124ad2979ee408f475c5ea1f1ec88c33c0774
DeathSpreads <68755892+SniffyMcTasty@users.noreply.github.com>
2023-03-10 12:00:33 -0500

    Fixed cornell box rendering

--- COMP371_RaytracerBase/code/src/RayTracer.cpp
+++ COMP371_RaytracerBase/code/src/RayTracer.cpp
@@ -200,7 +200,7 @@ color RayTracer::rayColor(Ray ray, OutputVariable *output, HittableList &hittabl
                 string lightType = light->getType();
                 if(lightType == "point" || (lightType == "area" && output->isGlobalIllum())) {
                     vec3 L = (light->getCentre() - rec.p).normalized();
-                    if (!hittableList.hitBeforeLight(Ray(rec.p, L), 0.01, std::numeric_limits<float>::infinity())) {
+                    if (!hittableList.hitBeforeLight(Ray(rec.p, L), 0.01, (light->getCentre() - rec.p).norm())) {
                         float lambertian = max(rec.normal.dot(L), (float) 0.0);
                         diffuseColor += intensities.at(0).cwiseProduct(rec.colors.at(1)) * rec.colorCoefficients.at(1) * lambertian;
                         if(!output->isGlobalIllum()) {
@@ -220,7 +220,7 @@ color RayTracer::rayColor(Ray ray, OutputVariable *output, HittableList &hittabl
         }
         color temp = ambientColor + diffuseColor + specularColor;
         if(maxBounce > 0) {
-            point3 target = rec.p + rec.normal + randomVectorInHemisphere(rec.normal);
+            point3 target = rec.p + randomVectorInHemisphere(rec.normal);
             temp = temp.cwiseProduct(rayColor(Ray(rec.p, target - rec.p), output, hittableList, lightVector, maxBounce-1));
         }
         return temp;
--- COMP371_RaytracerBase/code/src/RectangleGeometry.cpp
+++ COMP371_RaytracerBase/code/src/RectangleGeometry.cpp
@@ -30,7 +30,7 @@ string RectangleGeometry::toString() const
 
 bool RectangleGeometry::hit(const Ray &r, double t_min, double t_max, hit_record &rec) const
 {
-    vec3 normal = (this->p2 - this->p1).cross(this->p3 - this->p1);
+    vec3 normal = (this->p2 - this->p1).cross(this->p3 - this->p1).normalized();
     float d = normal.dot(this->p1);
     float denominator = normal.dot(r.getDirection());
     if(denominator == 0) return false;
@@ -38,31 +38,20 @@ bool RectangleGeometry::hit(const Ray &r, double t_min, double t_max, hit_record
     if(t < t_min || t > t_max) return false;
 
     point3 P = r.at(t);
-    vec3 C;
+    float outsideP1P2 = (this->p2 - this->p1).cross(P - this->p1).dot(normal);
+    if (outsideP1P2 < 0) return false;
 
-    vec3 edge1 = this->p2 - this->p1;
-    vec3 vp1 = P - this->p1;
-    C = edge1.cross(vp1);
-    if(normal.dot(C) < 0) return false;
+    float outsideP2P3 = (this->p3 - this->p2).cross(P - this->p2).dot(normal);
+    if (outsideP2P3 < 0) return false;
 
-    vec3 edge2 = this->p3 - this->p2;
-    vec3 vp2 = P - this->p2;
-    C = edge2.cross(vp2);
-    if(normal.dot(C) < 0) return false;
+    float outsideP3P1 = (this->p1 - this->p3).cross(P - this->p3).dot(normal);
+    if (outsideP3P1 < 0) {
+        float outsideP3P4 = (this->p4 - this->p3).cross(P - this->p3).dot(normal);
+        if (outsideP3P4 < 0) return false;
 
-    vec3 edgeMid = this->p1 - this->p3;
-    vec3 vp3 = P - this->p3;
-    C = edgeMid.cross(vp3);
-    if(normal.dot(C) < 0) {
-        vec3 edge3 = this->p4 - this->p3;
-        C = edge3.cross(vp3);
-        if(normal.dot(C) < 0) return false;
-
-        vec3 edge4 = this->p1 - this->p4;
-        vec3 vp4 = P - this->p4;
-        C = edge4.cross(vp4);
-        if(normal.dot(C) < 0) return false;
-    };
+        float outsideP4P1 = (this->p1 - this->p4).cross(P - this->p4).dot(normal);
+        if (outsideP4P1 < 0) return false;
+    }
 
     rec.t = t;
     rec.p = P;
